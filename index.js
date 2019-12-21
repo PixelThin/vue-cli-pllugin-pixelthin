0b758f891fa20ca2e4d510086cb6fa4e9bc4cfc5
Dave Beck <ajqajq@gmail.com>
2019-12-21 10:47:03 -0600

    Removes preset files. Adds a check for routes.json

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const path = require('path')
 const PrerenderSpaPlugin = require('prerender-spa-plugin')
+const fs = require('fs')
 
 module.exports = (api) => {
     api.configurewWebpack = function (config) {
+        const routeFile = api.resolve("src/router/routes.json")
+        const exists = fs.existsSync(routeFile)
+        const routes = exists ? require(routeFile) : [
+            { path: '/', preRender: true }
+        ]
+        if (!exists) {
+            console.log(`*****Warning: no routes file found at src/routers/routes.json.
+            Only the root path will be pre-rendered.`)
+        }
         return {
             plugins: [
                 new CopyWebpackPlugin([{
@@ -13,9 +23,8 @@ module.exports = (api) => {
                 new PrerenderSpaPlugin({
                     // Required - The path to the webpack-outputted app to prerender.
                     staticDir: path.join(__dirname, 'prod/dist'),
-                    // Required - Routes to render.
-                    // routes: ['/', '/about', '/some/deep/nested/route'],
-                    routes: routes.map(r => r.path)
+                    // The routes to render should be in an object 
+                    routes: routes.filter(r => r.preRender).map(r => r.path)
                 })
             ]
         }
